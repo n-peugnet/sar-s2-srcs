80bde4abf594673481d2b54798bbe343fe32cde5
n-peugnet <n.peugnet@free.fr>
2020-05-02 19:56:55 +0200

    TME7 q4

--- TME7/src/srcs/rmi/service/AbstractFunctionService.java
+++ TME7/src/srcs/rmi/service/AbstractFunctionService.java
@@ -1,7 +1,32 @@
 package srcs.rmi.service;
 
 import java.io.Serializable;
+import java.rmi.RemoteException;
 
 public abstract class AbstractFunctionService<P extends Serializable, R extends Serializable> implements FunctionService<P, R> {
+	
+	protected String name;
 
+	protected abstract R perform(P param) throws RemoteException;
+
+	public AbstractFunctionService(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String getName() throws RemoteException {
+		return name;
+	}
+
+	@Override
+	public R invoke(P param) throws RemoteException {
+		return perform(param);
+	}
+
+	@Override
+	public FunctionService<P, R> migrateTo(Host host) throws RemoteException {
+		return host.deployExistingService(getName(), this);
+	}
+	
+	
 }
