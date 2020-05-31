45c81f1722ca898d96d4445470828465dce1b67f
n-peugnet <n.peugnet@free.fr>
2020-05-31 11:28:21 +0200

    fix tests projet

--- Projet/tests/srcs/workflow/test/JobForTest.java
+++ Projet/tests/srcs/workflow/test/JobForTest.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import srcs.workflow.job.Job;
 
